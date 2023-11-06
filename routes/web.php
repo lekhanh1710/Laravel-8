28925a052fded333f42a0e35d71db7696e1c398a
KhanhLD <khanh.ld@hostingviet.vn>
2023-11-06 16:11:49 +0700

    KhanhLD:create view

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/users/create',[Controllers\UserController::class,'create']);
